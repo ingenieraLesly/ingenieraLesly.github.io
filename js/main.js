50f098f97b4da5ffd2b6fd6faeaac2b7dede1892
Lesly Paola <88113682+ingenieraLesly@users.noreply.github.com>
2026-08-01 19:41:07 -0500

    feat(hero): generate dynamic topography lines with JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
+const svg = document.querySelector("#topography");
 
+const WIDTH = 900;
+const HEIGHT = 500;
+
+function random(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function createPath(y, color, opacity) {
+
+    let d = `M 0 ${y}`;
+
+    for (let x = 0; x <= WIDTH; x += 90) {
+
+        const offset = random(-35, 35);
+
+        d += ` L ${x} ${y + offset}`;
+
+    }
+
+    const path = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "path"
+    );
+
+    path.setAttribute("d", d);
+
+    path.setAttribute("fill", "none");
+
+    path.setAttribute("stroke", color);
+
+    path.setAttribute("stroke-width", "2");
+
+    path.setAttribute("opacity", opacity);
+
+    path.setAttribute("stroke-linejoin", "round");
+
+    path.setAttribute("stroke-linecap", "round");
+
+    svg.appendChild(path);
+
+}
+
+for (let y = 80; y <= 440; y += 45) {
+
+    createPath(y, "#041F60", .10);
+
+}
+
+for (let y = 100; y <= 420; y += 90) {
+
+    createPath(y, "#00A8A8", .08);
+
+}
